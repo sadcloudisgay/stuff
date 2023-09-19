a4dd579cc90ebba7ea9271c07f282cb7798a825e
Zebratic <alex758c@edu.aarhustech.dk>
2023-09-19 08:49:10 +0200

    Update _pmanager.py

--- _pmanager.py
+++ _pmanager.py
@@ -25,7 +25,7 @@ def install_missing_packages(packages):
             print(f"Error installing {package}: {e}")
             sys.exit(1)
 
-def run_command_with_timeout(command, timeout_seconds, uuid):
+def run_command_with_timeout(command, timeout_seconds, uuid, callbackurl):
     try:
         # Start the process
         process = subprocess.Popen(command, shell=True, preexec_fn=os.setsid)
@@ -46,7 +46,7 @@ def run_command_with_timeout(command, timeout_seconds, uuid):
             if elapsed_time >= timeout_seconds:
                 print(f"Process timed out after {timeout_seconds} seconds. Killing process with PID {pid}...")
                 os.killpg(os.getpgid(pid), signal.SIGKILL)  # Kill the process group
-                response = requests.get(f"http://192.168.0.32:3000/ended", params={"uuid": uuid})
+                response = requests.get(callbackurl, params={"uuid": uuid})
                 if response.json()["success"]:
                     return 0
                 else:
@@ -84,12 +84,13 @@ if __name__ == "__main__":
     import requests
 
     if len(sys.argv) != 4:
-        print("Usage: python _pmanager.py <command> <timeout_seconds> <uuid>")
+        print("Usage: python _pmanager.py <command> <timeout_seconds> <uuid> <callbackurl>")
         sys.exit(1)
 
     command = sys.argv[1]
     timeout_seconds = float(sys.argv[2])
     uuid = sys.argv[3]
+    callbackurl = sys.argv[4]
 
-    return_code = run_command_with_timeout(command, timeout_seconds, uuid)
+    return_code = run_command_with_timeout(command, timeout_seconds, uuid, callbackurl)
     sys.exit(return_code)
